4a47272c0e599f63bd6d9c5fa867e17362f18100
Evan <Evancat77@gmail.com>
2022-07-11 13:10:38 -0400

    update catrom types

--- src/Types.d.ts
+++
@@ -1,1 +0,0 @@
-export type Knot = Vector2 | Vector3 | CFrame;
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,34 @@
-import Chain from "./Chain";
-import Spline from "./Spline";
+type ValidPointType = CFrame | Vector3 | Vector2;
 
-export { Spline };
-export { Chain };
+interface CatRom<T = ValidPointType> {
+	SolvePosition(t: number): Vector3;
+	SolveCFrame(t: number): CFrame;
+	SolveRotCFrame(t: number): CFrame;
+	SolveVelocity(t: number): Vector3;
+	SolveAcceleration(t: number): Vector3;
+	SolveTangent(t: number): Vector3;
+	SolveNormal(t: number): Vector3;
+	SolveBinormal(t: number): Vector3;
+	SolveCurvature(t: number): [number, Vector3];
+	SolveLength(a?: number, b?: number): number;
+
+	SolveUniformPosition(t: number): Vector3;
+	SolveUniformCFrame(t: number): CFrame;
+	SolveUniformRotCFrame(t: number): CFrame;
+	SolveUniformVelocity(t: number): Vector3;
+	SolveUniformAcceleration(t: number): Vector3;
+	SolveUniformTangent(t: number): Vector3;
+	SolveUniformNormal(t: number): Vector3;
+	SolveUniformBinormal(t: number): Vector3;
+	SolveUniformCurvature(t: number): [number, Vector3];
+	SolveUniformLength(a?: number, b?: number): number;
+
+	PrecomputeArcLengthParams(numINtervals?: number): void;
+}
+
+interface CatRomConstructor {
+	new <T = ValidPointType>(points: Array<T>, alpha?: number, tension?: number): CatRom<T>;
+}
+
+declare const CatRom: CatRomConstructor;
+export = CatRom;
